acf569b67b575a8b78ae479e1a8cec2568b662f6
AngeloGioacchino Del Regno <kholk11@gmail.com>
2020-08-03 18:15:46 +0200

    display-drivers: Import somc_panel driver from Open Devices k4.14
    This is an import of the somc_panel driver from the Open Devices
    kernel 4.14, as of the latest HEAD.

--- msm/msm_drv.c
+++ msm/msm_drv.c
@@ -51,6 +51,10 @@
 #include "sde_wb.h"
 #include "sde_dbg.h"
 
+#ifdef CONFIG_DRM_SDE_SPECIFIC_PANEL
+#include "../somc_panel/somc_panel_exts.h"
+#endif /* CONFIG_DRM_SDE_SPECIFIC_PANEL */
+
 /*
  * MSM driver version:
  * - 1.0.0 - initial interface
@@ -829,6 +833,10 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 	}
 
 	drm_kms_helper_poll_init(ddev);
+
+#ifdef CONFIG_DRM_SDE_SPECIFIC_PANEL
+	incell_driver_init(priv);
+#endif /* CONFIG_DRM_SDE_SPECIFIC_PANEL */
 
 	return 0;
 
