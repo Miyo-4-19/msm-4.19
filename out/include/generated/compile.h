06717ce9bffd62b21af1cd3e3a529aa1bd7d31f1
Pavel Dubrova <pashadubrova@gmail.com>
2021-11-05 13:57:32 +0200

    clk: qcom: Add the clock controller drivers for SDM845
    Add drivers for global, camera, debug, display, gpu and video
    clock controller support on SDM845.
    This snapshot of the camcc-sdm845, debugcc-sdm845, dispcc-sdm845,
    gcc-sdm845, gpucc-sdm845 and videocc-sdm845 drivers as of
    https://source.codeaurora.org/quic/la/kernel/msm-4.9
    commit 7e5c05f ("Merge "msm: camera: jpeg: Ensure in/out map entries are
    within allowed range"").
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Sep 14 22:10:23 +08 2024"
+#define LINUX_COMPILE_BY "ekkusa"
+#define LINUX_COMPILE_HOST "miyo"
+#define LINUX_COMPILER "Neutron clang version 19.0.0git (https://github.com/llvm/llvm-project.git fd3eaf76ba3392a4406247d996e757ef49f7a8b2), Neutron LLD 19.0.0"
